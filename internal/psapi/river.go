cc338b4b07335e4aa79e66ff054b26c2a711a01d
Michael Ko <gigaspyder@gmail.com>
2024-04-25 07:27:52 -0600

    ensure body gets closed in river reader

--- internal/psapi/river.go
+++ internal/psapi/river.go
@@ -68,177 +68,179 @@ func ConsumeRiver(f *CliFlags) {
 	backoffs := 0
 
 	for {
-		url := "https://api.pathofexile.com/public-stash-tabs"
-		if len(nextCursor) > 0 {
-			url = url + "?id=" + nextCursor
-		}
+		func() {
+			url := "https://api.pathofexile.com/public-stash-tabs"
+			if len(nextCursor) > 0 {
+				url = url + "?id=" + nextCursor
+			}
 
-		req, err := http.NewRequest("GET", url, nil)
-		req.Header.Add("Authorization", "Bearer "+os.Getenv("GGG_OAUTH_TOKEN"))
-		req.Header.Add("User-Agent", os.Getenv("GGG_USERAGENT"))
+			req, err := http.NewRequest("GET", url, nil)
+			req.Header.Add("Authorization", "Bearer "+os.Getenv("GGG_OAUTH_TOKEN"))
+			req.Header.Add("User-Agent", os.Getenv("GGG_USERAGENT"))
 
-		if err != nil {
-			log.Panic(err)
-		}
+			if err != nil {
+				log.Panic(err)
+			}
 
-		l.Println("Polling psapi...")
-		resp, err := client.Do(req)
-		reqHandleStart := time.Now()
-		if err != nil {
-			l.Printf("request errored out: %s\n", err)
-			if resp != nil {
-				l.Printf("Request failed: %s\n", resp.Status)
-				for k, v := range resp.Header {
-					l.Printf("  %s=%s\n", k, v)
+			l.Println("Polling psapi...")
+			resp, err := client.Do(req)
+			reqHandleStart := time.Now()
+			if err != nil {
+				l.Printf("request errored out: %s\n", err)
+				if resp != nil {
+					l.Printf("Request failed: %s\n", resp.Status)
+					for k, v := range resp.Header {
+						l.Printf("  %s=%s\n", k, v)
+					}
+				}
+				log.Panic(err)
+			}
+			defer resp.Body.Close()
+
+			rateLimitExceeded := false
+			// Handle rate limit
+			switch resp.StatusCode {
+			case 429:
+				l.Printf("psapi returned 429 (Too Many Requests)\n")
+				rateLimitExceeded = true
+				retryS, retryErr := strconv.Atoi(resp.Header.Get("Retry-After"))
+				if retryErr != nil {
+					l.Printf("failed to parse Retry-After header; panicing\n")
+					l.Printf("Headers:\n%+v\n", resp.Header)
+					log.Panic(retryErr)
+				}
+				nextWaitMs = retryS * 1000
+			case 200:
+				nextWaitMs = 0
+			}
+
+			// decode rate limit policy
+			rateLimitRules := strings.Split(resp.Header.Get("x-rate-limit-rules"), ",")
+
+			for _, rule := range rateLimitRules {
+				policyHeader := "x-rate-limit-" + rule
+				policyValues := strings.Split(resp.Header.Get(policyHeader), ":")
+				maxHits, maxHitsErr := strconv.Atoi(policyValues[0])
+				if maxHitsErr != nil {
+					l.Printf("Failed to decode rate-limit header - %s\n", maxHitsErr)
+					l.Printf("Headers:\n%+v\n", resp.Header)
+					respDump, _ := httputil.DumpResponse(resp, false)
+					l.Printf("Full response: \n%+v\n", respDump)
+					log.Panic(maxHitsErr)
+				}
+				periodS, periodErr := strconv.Atoi(policyValues[1])
+				if periodErr != nil {
+					l.Printf("Failed to decode rate-limit header - %s\n", periodErr)
+					l.Printf("Headers:\n%+v\n", resp.Header)
+					respDump, _ := httputil.DumpResponse(resp, false)
+					l.Printf("Full response: \n%+v\n", respDump)
+					log.Panic(periodErr)
+				}
+
+				ruleIntervalMs := (periodS * 1000) / maxHits
+
+				if ruleIntervalMs > nextWaitMs {
+					nextWaitMs = ruleIntervalMs
 				}
 			}
-			log.Panic(err)
-		}
 
-		rateLimitExceeded := false
-		// Handle rate limit
-		switch resp.StatusCode {
-		case 429:
-			l.Printf("psapi returned 429 (Too Many Requests)\n")
-			rateLimitExceeded = true
-			retryS, retryErr := strconv.Atoi(resp.Header.Get("Retry-After"))
-			if retryErr != nil {
-				l.Printf("failed to parse Retry-After header; panicing\n")
-				l.Printf("Headers:\n%+v\n", resp.Header)
-				log.Panic(retryErr)
+			currentCursor := nextCursor
+			nextCursor = resp.Header.Get("x-next-change-id")
+			if nextCursor != "" {
+				l.Printf("Next stash change id: %s\n", nextCursor)
 			}
-			nextWaitMs = retryS * 1000
-		case 200:
-			nextWaitMs = 0
-		}
-
-		// decode rate limit policy
-		rateLimitRules := strings.Split(resp.Header.Get("x-rate-limit-rules"), ",")
-
-		for _, rule := range rateLimitRules {
-			policyHeader := "x-rate-limit-" + rule
-			policyValues := strings.Split(resp.Header.Get(policyHeader), ":")
-			maxHits, maxHitsErr := strconv.Atoi(policyValues[0])
-			if maxHitsErr != nil {
-				l.Printf("Failed to decode rate-limit header - %s\n", maxHitsErr)
-				l.Printf("Headers:\n%+v\n", resp.Header)
-				respDump, _ := httputil.DumpResponse(resp, false)
-				l.Printf("Full response: \n%+v\n", respDump)
-				log.Panic(maxHitsErr)
-			}
-			periodS, periodErr := strconv.Atoi(policyValues[1])
-			if periodErr != nil {
-				l.Printf("Failed to decode rate-limit header - %s\n", periodErr)
-				l.Printf("Headers:\n%+v\n", resp.Header)
-				respDump, _ := httputil.DumpResponse(resp, false)
-				l.Printf("Full response: \n%+v\n", respDump)
-				log.Panic(periodErr)
+			if nextCursor == "" && nextWaitMs == 0 {
+				// We've reached the end, pause the reader (if it hasn't been paused already
+				l.Printf("No next change id\n")
+				nextWaitMs = 60
 			}
 
-			ruleIntervalMs := (periodS * 1000) / maxHits
-
-			if ruleIntervalMs > nextWaitMs {
-				nextWaitMs = ruleIntervalMs
+			type HeadResponse struct {
+				id  string
+				err error
 			}
-		}
+			headCh := make(chan HeadResponse, 1)
+			go func(ch chan HeadResponse) {
+				res, ninjaErr := poeninja.GetLatestPSChangeId(client)
+				ch <- HeadResponse{res, ninjaErr}
+			}(headCh)
 
-		currentCursor := nextCursor
-		nextCursor = resp.Header.Get("x-next-change-id")
-		if nextCursor != "" {
-			l.Printf("Next stash change id: %s\n", nextCursor)
-		}
-		if nextCursor == "" && nextWaitMs == 0 {
-			// We've reached the end, pause the reader (if it hasn't been paused already
-			l.Printf("No next change id\n")
-			nextWaitMs = 60
-		}
-
-		type HeadResponse struct {
-			id  string
-			err error
-		}
-		headCh := make(chan HeadResponse, 1)
-		go func(ch chan HeadResponse) {
-			res, err := poeninja.GetLatestPSChangeId(client)
-			ch <- HeadResponse{res, err}
-		}(headCh)
-
-		decodeStart := time.Now()
-		tabs, decodeErr := FindFFJewels(resp.Body, l, currentCursor)
-		if decodeErr != nil && decodeErr != io.EOF {
-			log.Panic(decodeErr)
-		}
-		decodeEnd := time.Since(decodeStart)
-
-		l.Printf("Response: processed %d stash tabs in %s\n", len(tabs), decodeEnd)
-		ctx := context.TODO()
-
-		// Slowly back off if we're at the front of the river
-		if len(tabs) == 0 && !rateLimitExceeded {
-			nextWaitMs = nextWaitMs * IntPow(2, backoffs)
-			if backoffs < MAX_BACKOFFS {
-				backoffs++
+			decodeStart := time.Now()
+			tabs, decodeErr := FindFFJewels(resp.Body, l, currentCursor)
+			if decodeErr != nil && decodeErr != io.EOF {
+				log.Panic(decodeErr)
 			}
-		} else if len(tabs) > 0 {
-			backoffs = 0
-			dbStart := time.Now()
-			// TODO: make this a goroutine? or if it's really slow, add a message broker here
-			err = UpdateDb(ctx, dbHandle, tabs)
+			decodeEnd := time.Since(decodeStart)
+
+			l.Printf("Response: processed %d stash tabs in %s\n", len(tabs), decodeEnd)
+			ctx := context.TODO()
+
+			// Slowly back off if we're at the front of the river
+			if len(tabs) == 0 && !rateLimitExceeded {
+				nextWaitMs = nextWaitMs * IntPow(2, backoffs)
+				if backoffs < MAX_BACKOFFS {
+					backoffs++
+				}
+			} else if len(tabs) > 0 {
+				backoffs = 0
+				dbStart := time.Now()
+				// TODO: make this a goroutine? or if it's really slow, add a message broker here
+				err = UpdateDb(ctx, dbHandle, tabs)
+				if err != nil {
+					log.Panic(err)
+				}
+				dbEnd := time.Since(dbStart)
+				l.Printf("Response: database updated in %s\n", dbEnd)
+			}
+
+			reqHandleEnd := time.Since(reqHandleStart)
+
+			headRes := <-headCh
+			if headRes.err != nil {
+				l.Printf("could not fetch latest change id: %s\n", headRes.err)
+				log.Panic(headRes.err)
+			}
+
+			// calculate drift from the river head
+			drift, err := CalculateRiverDrift(headRes.id, currentCursor)
 			if err != nil {
+				l.Printf("failed to calculate river drift: %s\n", err)
 				log.Panic(err)
 			}
-			dbEnd := time.Since(dbStart)
-			l.Printf("Response: database updated in %s\n", dbEnd)
-		}
 
-		reqHandleEnd := time.Since(reqHandleStart)
+			if len(tabs) > 0 {
+				c := db.DBChangeset{
+					ChangeId:     currentCursor,
+					NextChangeId: nextCursor,
+					StashCount:   len(tabs),
+					// TODO: make sure all timestamps are consistent
+					ProcessedAt:   decodeStart,
+					TimeTakenMs:   reqHandleEnd.Milliseconds(),
+					DriftFromHead: drift,
+				}
 
-		headRes := <-headCh
-		if headRes.err != nil {
-			l.Printf("could not fetch latest change id: %s\n", headRes.err)
-			log.Panic(headRes.err)
-		}
-
-		// calculate drift from the river head
-		drift, err := CalculateRiverDrift(headRes.id, currentCursor)
-		if err != nil {
-			l.Printf("failed to calculate river drift: %s\n", err)
-			log.Panic(err)
-		}
-
-		if len(tabs) > 0 {
-			c := db.DBChangeset{
-				ChangeId:     currentCursor,
-				NextChangeId: nextCursor,
-				StashCount:   len(tabs),
-				// TODO: make sure all timestamps are consistent
-				ProcessedAt:   decodeStart,
-				TimeTakenMs:   reqHandleEnd.Milliseconds(),
-				DriftFromHead: drift,
+				_, err = dbHandle.Exec(ctx, "INSERT INTO changesets(changeId,nextChangeId,stashCount,processedAt,timeTaken,driftFromHead) VALUES (@changeId,@nextChangeId,@stashCount,@processedAt,@timeTaken,@driftFromHead)", pgx.NamedArgs{
+					"changeId":      c.ChangeId,
+					"nextChangeId":  c.NextChangeId,
+					"stashCount":    c.StashCount,
+					"processedAt":   c.ProcessedAt,
+					"timeTaken":     c.TimeTakenMs,
+					"driftFromHead": c.DriftFromHead,
+				})
+				if err != nil {
+					log.Panic(err)
+				}
 			}
 
-			_, err = dbHandle.Exec(ctx, "INSERT INTO changesets(changeId,nextChangeId,stashCount,processedAt,timeTaken,driftFromHead) VALUES (@changeId,@nextChangeId,@stashCount,@processedAt,@timeTaken,@driftFromHead)", pgx.NamedArgs{
-				"changeId":      c.ChangeId,
-				"nextChangeId":  c.NextChangeId,
-				"stashCount":    c.StashCount,
-				"processedAt":   c.ProcessedAt,
-				"timeTaken":     c.TimeTakenMs,
-				"driftFromHead": c.DriftFromHead,
-			})
-			if err != nil {
-				log.Panic(err)
+			if nextWaitMs > 0 {
+				waitDuration := time.Duration(nextWaitMs)*time.Millisecond - reqHandleEnd
+				if waitDuration < 0 {
+					waitDuration = 0
+				}
+				l.Printf("waiting %s...\n", waitDuration)
+				time.Sleep(waitDuration)
 			}
-		}
-
-		if nextWaitMs > 0 {
-			waitDuration := time.Duration(nextWaitMs)*time.Millisecond - reqHandleEnd
-			if waitDuration < 0 {
-				waitDuration = 0
-			}
-			l.Printf("waiting %s...\n", waitDuration)
-			time.Sleep(waitDuration)
-		}
-		resp.Body.Close()
+		}()
 	}
 }
 
